9967135c8072e77d28849cf2f837b2df4c36312c
aniteddy <aniteddy1999@gmail.com>
2021-11-22 00:12:21 +0200

    master

--- mylibrary/views.py
+++ mylibrary/views.py
@@ -1,3 +1,36 @@
+from django.http import request
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+
+from mylibrary.models import Authors, Genres, Books, Publishers
+from mylibrary.serializers import AuthorsSerializer, GenresSerializer,BooksSerializer,PublishersSerializer
 
 # Create your views here.
+
+@csrf_exempt
+def BooksApi(reques, id=0):
+    if request.method=='GET':
+        books = Books.objects.all()
+        books_serializer = BooksSerializer(books,many=True)
+        return JsonResponse(books_serializer.data, safe=False)
+    elif request.method=='POST':
+        books_data=JSONParser().parse(request)
+        books_serializer= BooksSerializer(data=books_data)
+        if books_serializer.is_valid():
+            books_serializer.save()
+            return JsonResponse('Added Successfully', safe=False)
+        return JsonResponse('Failed to Add', safe=False)
+    elif request.method=='PUT':
+        books_data=JSONParser().parse(request)
+        books = Books,object.get(Id = books_data['Id'])
+        books_serializer=BooksSerializer(books,data=books_data)
+        if books_serializer.is_valid():
+            books_serializer.save()
+            return JsonResponse('Update Successfully', safe=False)
+        return JsonResponse('Failed to Update', safe=False)
+    elif request.method=='DELETE':
+        books = Books.objects.get(Id=id)
+        books.delete()
+        return JsonResponse('Delete Successfully', safe=False)
