a57b49c7f412bafeddc0681b0b21f9026f873f07
aniteddy <aniteddy1999@gmail.com>
2021-11-22 21:22:19 +0200

    master

--- mylibrary/urls.py
+++ mylibrary/urls.py
@@ -5,8 +5,8 @@ from mylibrary import views
 urlpatterns=[
 
     url(r'^book$', views.BooksAPI),
-    url(r'book/<int:pk>/', views.BooksAPIID),
-    #url(r'^book/([0-9]+)$', views.BooksAPI),
+    #url(r'book/<int:pk>/', views.BooksAPIID),
+    url(r'^book/(?P<pk>[0-9]+)/$', views.BooksAPIID),
     url(r'^author$', views.AuthorsAPI),
     url(r'^author/([0-9]+)$', views.AuthorsAPI),
     url(r'^genre$', views.GenresAPI),
